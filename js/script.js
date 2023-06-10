2eea2f44b5b236dd08c7cfd6178d6543e0adf3de
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-09 21:56:29 -0400

    adjusted all button styles to be the same, but with start button set to fit-content for width

--- js/script.js
+++ js/script.js
@@ -1,50 +1,48 @@
 var currentQuestionIndex = 0;
 
-// Building a function to show the question. Will try to comment my code as I go and then remove comments after committing the changes.
+
 function showQuestion() {
-    // variable to select the element with id of question
     var questionEl = document.querySelector("#question");
-    // reset innerHTML
     questionEl.innerHTML = "";
-    // variable to select the current object from the questionData array
     var questionDataObj = questionData[currentQuestionIndex];
-    // create the h2 to hold the question
     var questionH2 = document.createElement("h2");
 
-    // h2 text should pull from questionDataObj and append to the questionEl
     questionH2.innerText = questionDataObj.question;
     questionEl.append(questionH2);
 }
 
-// Function to show the choices in buttons.
+
 function showChoices() {
     var questionChoicesEl = document.querySelector("#choices");
     questionChoicesEl.innerHTML = "";
     var questionDataObj = questionData[currentQuestionIndex];
-  
+
     var choicesList = document.createElement("ol");
-  
+
     for (var i = 0; i < questionDataObj.choices.length; i++) {
-      var choice = questionDataObj.choices[i];
-  
-      var listItem = document.createElement("li");
-      var btn = document.createElement("button");
-      btn.style.width = "50%";
-      btn.style.backgroundColor = "#301934";
-      btn.style.color = "#fff";
-  
-      btn.innerText = choice;
-  
-      listItem.appendChild(btn);
-      choicesList.appendChild(listItem);
+        var choice = questionDataObj.choices[i];
+
+        var listItem = document.createElement("li");
+        var btn = document.createElement("button");
+
+        btn.innerText = choice;
+        btn.classList.add("button-style");
+
+        listItem.appendChild(btn);
+        choicesList.appendChild(listItem);
     }
-  
+
     questionChoicesEl.innerHTML = "";
     questionChoicesEl.appendChild(choicesList);
-  }
-  
+}
 
+
+function applyButtonStyle() {
+    var startButton = document.getElementById("startButton");
+    startButton.classList.add("button-style");
+}
 
 
 showQuestion();
 showChoices();
+applyButtonStyle();
